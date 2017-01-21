68df81fe0516237ba0096f1bc569f218dd0df2f5
Rimgon <drakefyre2000@gmail.com>
2017-01-21 12:02:45 -0500

    Cleaned up from the merge. Master now includes pressure sensor, rangefinder, and the dual wheel shooter

--- src/org/usfirst/frc/team4611/robot/OI.java
+++ src/org/usfirst/frc/team4611/robot/OI.java
@@ -3,11 +3,9 @@ package org.usfirst.frc.team4611.robot;
 
 //import org.usfirst.frc.team4611.robot.commands.ButtonFast;
 //import org.usfirst.frc.team4611.robot.commands.ButtonMed;
-<<<<<<< HEAD
-import org.usfirst.frc.team4611.robot.commands.ButtonSlow;
-=======
+
 import org.usfirst.frc.team4611.robot.commands.*;
->>>>>>> Sensors
+
 
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.buttons.Button;
@@ -22,18 +20,8 @@ public class OI {
     //INSTANTIATING JOYSTICKS AND BUTTONS
 
     //Instantiate Joysticks on ports 0 and 1.
-<<<<<<< HEAD
-    public Joystick leftJoy = new Joystick(0);
-    public Joystick rightJoy = new Joystick(1);
-    public Button slow= new JoystickButton(leftJoy, 4);
-	//public Button med = new JoystickButton(leftJoy, 3);
-	//public Button fast= new JoystickButton(leftJoy, 5);
-    
-    public OI() {
-    	this.slow.whileHeld(new ButtonSlow());
-		//this.med.whileHeld(new ButtonMed());
-		//this.fast.whileHeld(new ButtonFast());
-=======
+
+
     public Joystick leftJoy;
     public Joystick rightJoy;
     public Button shootbut;
@@ -45,7 +33,7 @@ public class OI {
         shootbut = new JoystickButton(leftJoy, 1);
     	//shootbut.whileHeld (new DualWheelShooter());
     	
->>>>>>> Sensors
+
 
     }
 
