49252b599eb69607fad96b19f56d39c1bc19ec7e
Connor2573 <connor.park2573@gmail.com>
2017-01-23 17:12:39 -0500

    Gyro code V2

--- src/org/usfirst/frc/team4611/robot/commands/Gyro.java
+++ src/org/usfirst/frc/team4611/robot/commands/Gyro.java
@@ -1,33 +1,35 @@
 package org.usfirst.frc.team4611.robot.commands;
-
-
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.RobotDrive;
 import edu.wpi.first.wpilibj.SampleRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-public class Gyro extends SampleRobot {
 
-	private RobotDrive myRobot; // robot drive system
-    private AnalogGyro gyro = new AnalogGyro(1);// Gyro on Analog Channel 1
-
+public class Gyro {
+	
+    private AnalogGyro gyro = new AnalogGyro(2);// This line MUST be changed
+    //the type of gyro we have is not analog based, so the constructor to be used needs a model #
     double Kp = 0.03; // how fast the turns are, Too fast = oscillation, too slow = may not reach destination
-
+    double angle;
+    double correctionAngle;
+    
+    
+    
     public Gyro() {
-                 
-        myRobot = new RobotDrive(0,1); // Drive train roborio on PWM 0 and 1
-        myRobot.setExpiration(0.1); //0.1 expiration timeout
-        }
-
-    public void autonomous() {
-        gyro.reset();
-        while (isAutonomous()) {
-            double angle = gyro.getAngle(); // get current heading
-            myRobot.drive(-1.0, -angle*Kp); // drive towards heading 0
-            Timer.delay(0.004); 
-            SmartDashboard.putNumber("Angle", angle);
-        }
-        myRobot.drive(0.0, 0.0); //Stops Robot
+    	gyro.reset();//angle set to 0 when restarted
     }
-}
+    
+    public void gyroMeasure()
+    {
+    	angle = gyro.getAngle();
+    	correctionAngle = -angle * Kp;
+    	
+    	SmartDashboard.putNumber("Current Heading", angle);
+    	SmartDashboard.putNumber("Calculation from wpilib", correctionAngle);
+    	
+    }
+
+   
+    }
+
