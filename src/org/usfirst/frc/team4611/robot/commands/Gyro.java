d09e87c0944f4f285e94b6c093deffa9852149ac
MaddyMcGee <maddymcgee@yahoo.com>
2017-01-27 15:40:58 -0500

    used ADXRS450 gyro liibrary instead of writing a class

--- src/org/usfirst/frc/team4611/robot/commands/Gyro.java
+++ src/org/usfirst/frc/team4611/robot/commands/Gyro.java
@@ -1,5 +1,5 @@
 package org.usfirst.frc.team4611.robot.commands;
-import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -7,7 +7,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Gyro {
 	
-    private ADXRS450Gyro gyro = new ADXRS450Gyro();
+    private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
     double Kp = 0.03; // how fast the turns are, Too fast = oscillation, too slow = may not reach destination
     double angle;
     double correctionAngle;
@@ -15,13 +15,11 @@ public class Gyro {
     
     
     public Gyro() {
-    	gyro.startThread();
     	gyro.reset();//angle set to 0 when restarted
     }
     
     public void gyroMeasure()
     {
-    	gyro.update();
     	angle = gyro.getAngle();
     	correctionAngle = -angle * Kp;
     	
