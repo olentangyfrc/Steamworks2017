233e7e6763bf62d264961c46b33c5715b56c58ab
MaddyMcGee <maddymcgee@yahoo.com>
2017-01-28 10:11:38 -0500

    added some bomb a** calculations

--- src/org/usfirst/frc/team4611/robot/commands/Gyro.java
+++ src/org/usfirst/frc/team4611/robot/commands/Gyro.java
@@ -1,7 +1,5 @@
 package org.usfirst.frc.team4611.robot.commands;
-import edu.wpi.first.wpilibj.ADXRS450_Gyro;
-import edu.wpi.first.wpilibj.SPI;
-import edu.wpi.first.wpilibj.SPI.Port;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro; //uses acceleration to measure the horizontal position of the gyro relative to angle measure 0
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
@@ -9,6 +7,9 @@ public class Gyro {
 	
     private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
     double raw;
+    int angleRound;
+    int heading;
+    int rotations;
     
     
     
@@ -18,8 +19,20 @@ public class Gyro {
     
     public void gyroMeasure()
     {
-    	raw = gyro.getAngle();    	
+    	raw = gyro.getAngle();//get's the angle of the gyro relative to the set angle 0
+    	//negative values counter clockwise, positive values clockwise
+    	angleRound = (int)(raw + .5);
+    	heading = angleRound;
+    	if(heading < -360 || heading > 360)
+    	{
+    		heading = 0;
+    	}
+    	rotations %= angleRound;
+    	
     	SmartDashboard.putNumber("Raw Heading", raw);
+    	SmartDashboard.putNumber("Rounded Heading", angleRound);
+    	SmartDashboard.putNumber("Heading [-360,360]", heading);
+    	SmartDashboard.putNumber("# of roations", rotations);
     	
     }
 
