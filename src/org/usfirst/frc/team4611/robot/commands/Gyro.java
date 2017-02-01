c02c882bc1b981faf53ed69839a436b36c5ae96e
MaddyMcGee <maddymcgee@yahoo.com>
2017-02-01 16:11:49 -0500

    tidied up code even more for Hannugh

--- src/org/usfirst/frc/team4611/robot/commands/Gyro.java
+++ src/org/usfirst/frc/team4611/robot/commands/Gyro.java
@@ -8,7 +8,7 @@ public class Gyro {
     private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
     double raw;//raw heading, no math
     int angleRound;//nearest whole number of the heading
-    int heading;//the angle the robot is facing
+    int heading;//the angle the robot is facing, between -360 & 360
     int rotations;//# of times the robot has rotated
     
     
@@ -19,18 +19,19 @@ public class Gyro {
     public void gyroMeasure(){
     	raw = gyro.getAngle();//gets the angle of the gyro relative to the set angle 0
     	//negative values counter clockwise, positive values clockwise
+    	
     	angleRound = (int)Math.round(raw);
+    	
     	heading = angleRound;
+    	//the remainder of angle/360 is an equivalent angle, just a simpler number to think with
     	if(heading <= -360 || heading >= 360)
     	{
     		heading %= 360;
     	}
+    	
     	rotations = angleRound / 360;
     	
-    	//SmartDashboard.putNumber("Raw Heading", raw);
-    	//SmartDashboard.putNumber("Rounded Heading", angleRound);
     	SmartDashboard.putNumber("Heading [-360,360]", heading);//Shows accurate angle
-    	//SmartDashboard.putNumber("# of roations", rotations);
     	
     }
 
