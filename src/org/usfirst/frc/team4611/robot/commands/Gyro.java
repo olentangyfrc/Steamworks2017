2c178a5a539df76354d9fa4a63aded915931cae7
MaddyMcGee <maddymcgee@yahoo.com>
2017-02-01 16:03:12 -0500

    tidied up code, removed unnecessary variables, added comments, ect.

--- src/org/usfirst/frc/team4611/robot/commands/Gyro.java
+++ src/org/usfirst/frc/team4611/robot/commands/Gyro.java
@@ -1,37 +1,28 @@
 package org.usfirst.frc.team4611.robot.commands;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro; //uses acceleration to measure the horizontal position of the gyro relative to angle measure 0
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 
 public class Gyro {
 	
     private ADXRS450_Gyro gyro = new ADXRS450_Gyro();
-    double raw;
-    int angleRound;
-    int heading;
-    int rotations;
-    boolean direction;//true is clockwise
+    double raw;//raw heading, no math
+    int angleRound;//nearest whole number of the heading
+    int heading;//the angle the robot is facing
+    int rotations;//# of times the robot has rotated
     
     
     public Gyro() {
     	gyro.reset();//angle set to 0 when restarted
     }
     
-    public void gyroMeasure()
-    {
-    	raw = gyro.getAngle();//get's the angle of the gyro relative to the set angle 0
+    public void gyroMeasure(){
+    	raw = gyro.getAngle();//gets the angle of the gyro relative to the set angle 0
     	//negative values counter clockwise, positive values clockwise
     	angleRound = (int)Math.round(raw);
     	heading = angleRound;
-    	if(heading <= -360)
+    	if(heading <= -360 || heading >= 360)
     	{
-    		direction = false;
-    		heading %= 360;
-    	}
-    	if(heading >= 360)
-    	{
-    		direction = true;
     		heading %= 360;
     	}
     	rotations = angleRound / 360;
@@ -40,8 +31,6 @@ public class Gyro {
     	//SmartDashboard.putNumber("Rounded Heading", angleRound);
     	SmartDashboard.putNumber("Heading [-360,360]", heading);//Shows accurate angle
     	//SmartDashboard.putNumber("# of roations", rotations);
-    	//SmartDashboard.putBoolean("Clockwise?", direction);
-    	//LiveWindow.addSensor("Gyro", "gyroscope", gyro);
     	
     }
 
