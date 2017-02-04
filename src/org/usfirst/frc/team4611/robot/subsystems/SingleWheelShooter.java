28538baae19a6473c960423e2a85fbb07cbc6573
sethstobart <seth.stobart@gmail.com>
2017-02-04 10:13:16 -0500

    Errors but I gotta switch branches

--- src/org/usfirst/frc/team4611/robot/subsystems/SingleWheelShooter.java
+++ src/org/usfirst/frc/team4611/robot/subsystems/SingleWheelShooter.java
@@ -3,15 +3,17 @@ package org.usfirst.frc.team4611.robot.subsystems;
 import org.usfirst.frc.team4611.robot.RobotMap;
 import org.usfirst.frc.team4611.robot.commands.SingleWheelShoot;
 
+import com.ctre.CANTalon;
+
 import edu.wpi.first.wpilibj.Victor;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class SingleWheelShooter extends Subsystem{
 	
-	private Victor singleWheelShooter;
+	private CANTalon singleWheelShooter;
 	
 	public SingleWheelShooter(){
-		this.singleWheelShooter = new Victor (RobotMap.singleShooter);
+		this.singleWheelShooter = new CANTalon (RobotMap.singleShooter);
 	}
 	public void shoot (double speed){
 		this.singleWheelShooter.set(speed);
