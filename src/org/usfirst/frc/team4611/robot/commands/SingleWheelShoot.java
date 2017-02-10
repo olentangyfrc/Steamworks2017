28ae49438ee3a8987dbdc3003d5086ac8ff90296
Hannah He <hannahhe18@gmail.com>
2017-02-10 16:54:11 -0500

    made z-axis the speed controller

--- src/org/usfirst/frc/team4611/robot/commands/SingleWheelShoot.java
+++ src/org/usfirst/frc/team4611/robot/commands/SingleWheelShoot.java
@@ -17,9 +17,9 @@ public class SingleWheelShoot extends Command{
 	 protected void initialize() {
 	    }
 	 protected void execute() {
-	    // double joyVal = (Robot.oi.rightJoy.getZ()*0.4); //port one //no filter on this (run 40%) //Use the 'filter' function on the raw joystick input
-	     //Robot.sw.shoot(Robot.oi.leftJoy.getZ()); //Actually pass that value to the motors
-	     Robot.sw.shoot(RobotMap.singleShooterSpeed); //button only
+	    double joyVal = (Robot.oi.rightJoy.getZ()); //port one //no filter on this (run 40%) //Use the 'filter' function on the raw joystick input
+	     Robot.sw.shoot(joyVal); //Actually pass that value to the motors
+	     //Robot.sw.shoot(RobotMap.singleShooterSpeed); //button only
 	    }
 	@Override
 	protected boolean isFinished() {
