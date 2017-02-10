f61b6ea1bdf6e98761589a29bb91ccdd6ff3d9a7
MaddyMcGee <maddymcgee@yahoo.com>
2017-02-10 17:24:13 -0500

    adding files

---
+++ src/org/usfirst/frc/team4611/robot/commands/AccelMeasure.java
@@ -0,0 +1,30 @@
+package org.usfirst.frc.team4611.robot.commands;
+
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
+import edu.wpi.first.wpilibj.interfaces.Accelerometer;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class AccelMeasure {
+	
+	Accelerometer accel;
+	double xVal;
+	double yVal;
+	double zVal;
+	
+	
+	public AccelMeasure(){
+	accel = new BuiltInAccelerometer(); 
+	//accel = new BuiltInAccelerometer(Accelerometer.Range.k4G); 
+
+	}
+	
+	public void accelMeasurement(){
+		xVal = accel.getX();
+		yVal = accel.getY();
+		zVal = accel.getZ();
+		
+		SmartDashboard.putNumber("x acceleration", xVal);
+		SmartDashboard.putNumber("y acceleration", yVal);
+		SmartDashboard.putNumber("z acceleration", zVal);
+	}
+}
