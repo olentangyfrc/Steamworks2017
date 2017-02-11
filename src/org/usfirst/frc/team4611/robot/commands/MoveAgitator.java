a4a7d58ebefad94d48b339e3a5b464de401c3d3a
Rimgon <drakefyre2000@gmail.com>
2017-02-11 16:09:57 -0500

    I'm stoopid. Added new files

---
+++ src/org/usfirst/frc/team4611/robot/commands/MoveAgitator.java
@@ -0,0 +1,43 @@
+   package org.usfirst.frc.team4611.robot.commands;
+
+import org.usfirst.frc.team4611.robot.Robot;
+import org.usfirst.frc.team4611.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class MoveAgitator extends Command {
+	
+	private double speed;
+		
+	public MoveAgitator(double inputspeed){
+		this.speed = inputspeed;
+		this.requires(Robot.ag);
+	}
+	public MoveAgitator(){
+		this.requires(Robot.ag);
+	}
+	protected void initialize(){
+		//Runs once, on initialization.
+	}
+	
+	protected void execute(){
+		RobotMap.agitateSpeed = (SmartDashboard.getNumber("agitate speed", 0)) / 100;
+		Robot.ag.agitate(RobotMap.agitateSpeed);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	protected void end(){
+		Robot.fe.feed(0);
+	}
+
+	protected void interupted(){
+		this.end();
+	}
+
+}
