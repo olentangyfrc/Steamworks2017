c44638a9876d085a458dc80ea65860a8a1b5f200
sethstobart <seth.stobart@gmail.com>
2017-02-15 13:26:15 -0500

    Added the code from the CTRE git

--- src/org/usfirst/frc/team4611/robot/Robot.java
+++ src/org/usfirst/frc/team4611/robot/Robot.java
@@ -9,10 +9,16 @@ import org.usfirst.frc.team4611.robot.subsystems.*;
 import org.usfirst.frc.team4611.robot.subsystems.leftSide;
 import org.usfirst.frc.team4611.robot.subsystems.rightSide;
 
+import com.ctre.CANTalon;
+import com.ctre.CANTalon.FeedbackDevice;
+import com.ctre.CANTalon.TalonControlMode;
+
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.IterativeRobot;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.NamedSendable;
 import edu.wpi.first.wpilibj.Preferences;
+import edu.wpi.first.wpilibj.Joystick.AxisType;
 import edu.wpi.first.wpilibj.command.Command;
 import edu.wpi.first.wpilibj.command.Scheduler;
 import edu.wpi.first.wpilibj.livewindow.LiveWindow;
@@ -39,6 +45,14 @@ public class Robot extends IterativeRobot {
 	public UltrasonicRange ultra;
 	public Gyro gy;
 	public static boolean dir = false;
+	
+	CANTalon _talon = new CANTalon(48);	
+	Joystick _joy = new Joystick(0);	
+	StringBuilder _sb = new StringBuilder();
+	private FeedbackDevice shooterEncoder;
+	int _loops = 0;
+	boolean _lastButton1 = false;
+	double targetPositionRotations;
 
 	public static Preferences prefs ;
 	Command autonomousCommand;
@@ -71,6 +85,32 @@ public class Robot extends IterativeRobot {
 		// this.autonomousCommand = new autonomousCommandGroup();
 		// table = NetworkTable.getTable("GRIP/data"); //Network tables to pull
 		// VA data to roborio. Not currently in use
+		
+		/* lets grab the 360 degree position of the MagEncoder's absolute position */
+		int absolutePosition = _talon.getPulseWidthPosition() & 0xFFF; /* mask out the bottom12 bits, we don't care about the wrap arounds */
+        /* use the low level API to set the quad encoder signal */
+        _talon.setEncPosition(absolutePosition);
+        
+        /* choose the sensor and sensor direction */
+        shooterEncoder = (FeedbackDevice.QuadEncoder);
+        _talon.setFeedbackDevice(shooterEncoder);
+        _talon.reverseSensor(true);
+        _talon.configEncoderCodesPerRev(20);
+
+        /* set the peak and nominal outputs, 12V means full */
+        _talon.configNominalOutputVoltage(+0f, -0f);
+        _talon.configPeakOutputVoltage(+12f, -12f);
+        /* set the allowable closed-loop error,
+         * Closed-Loop output will be neutral within this range.
+         * See Table in Section 17.2.1 for native units per rotation. 
+         */
+        _talon.setAllowableClosedLoopErr(0); /* always servo */
+        /* set closed loop gains in slot0 */
+        _talon.setProfile(0);
+        _talon.setF(-5.115);
+        _talon.setP(0.0);
+        _talon.setI(0.0); 
+        _talon.setD(0.0);          
 	}
 
 	/**
@@ -139,7 +179,42 @@ public class Robot extends IterativeRobot {
 		// LiveWindow.run();
 		ultra.ultrasonicMeasurement();
 		sw.getEncoderMeasure();
-	}
+		
+		/* get gamepad axis */
+    	double leftZstick = _joy.getAxis(AxisType.kZ);
+    	double motorOutput = _talon.getOutputVoltage() / _talon.getBusVoltage();
+    	//boolean button1 = _joy.getRawButton(1);
+    	//boolean button2 = _joy.getRawButton(2);
+    	/* prepare line to print */
+		_sb.append("\tout:");
+		_sb.append(motorOutput);
+        _sb.append("\tspd:");
+        _sb.append(_talon.getSpeed());
+        
+        /* on button1 press enter closed-loop mode on target position */
+        if(true || _joy.getRawButton(1)) {
+        	//double targetSpeed = leftZstick * -3950;
+        	double targetSpeed = -1500;
+        	_talon.changeControlMode(TalonControlMode.Speed);
+        	_talon.set(targetSpeed); 	
+        	
+        	//_sb.append("\taxis");
+        	_sb.append(leftZstick);
+        	_sb.append("\terr:");
+        	_sb.append(_talon.getClosedLoopError());
+        	_sb.append("\ttrg:");
+        	_sb.append(-targetSpeed);
+        }
+        else {
+        	_talon.changeControlMode(TalonControlMode.PercentVbus);
+        	_talon.set(-0.5);
+        }
+        if(++_loops >= 10) {
+        	_loops = 0;
+        	System.out.println(_sb.toString());
+        }
+        _sb.setLength(0);
+    }
 
 	/**
 	 * This function is called periodically during test mode
