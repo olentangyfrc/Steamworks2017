4f5cc1c05b1a70ff666fc9480eaa9816deb6790e
Rimgon <drakefyre2000@gmail.com>
2017-02-18 09:23:28 -0500

    Fixed the agitator. Please don't comment  out and then push that to master. Please.

--- src/org/usfirst/frc/team4611/robot/commands/MoveAgitator.java
+++ src/org/usfirst/frc/team4611/robot/commands/MoveAgitator.java
@@ -22,8 +22,7 @@ public class MoveAgitator extends Command {
 	}
 	
 	protected void execute(){
-		//RobotMap.agitateSpeed = (SmartDashboard.getNumber("agitate speed", 0)) / 100;
-		
+		RobotMap.agitateSpeed = (SmartDashboard.getNumber("agitate speed", 0)) / 100;
 		Robot.ag.agitate(RobotMap.agitateSpeed);
 	}
 	
