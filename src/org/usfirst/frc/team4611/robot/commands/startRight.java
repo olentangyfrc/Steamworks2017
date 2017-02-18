f01f40f72fceef1b4b148a2b26ed9e4a93b8b643
Hannah He <hannahhe18@gmail.com>
2017-02-18 15:58:48 -0500

    Closest we have ever been. VA needs refining

--- src/org/usfirst/frc/team4611/robot/commands/startRight.java
+++ src/org/usfirst/frc/team4611/robot/commands/startRight.java
@@ -6,10 +6,15 @@ public class startRight extends CommandGroup{
 	
 	public startRight(){
 		addSequential(new driveAuto(-0.5),0.75);
-    	addSequential(new turnAuto(-1, 0.5), 0.6);
-    	addSequential(new driveAuto(-0.5), 0.8);
-    	addSequential(new driveAuto(0));
-    	addSequential(new autoAim());
+    	addSequential(new turnAuto(-1, 0.45), 0.6);
+    	//addSequential(new driveAuto(-0.5), 0.8);
+    	//addSequential(new driveAuto(0), 0.1);
+    	addSequential(new driveAuto(-0.5), 0.5);
+    	addSequential (new driveAuto(0),0.1);
+    	addSequential(new autoAim(0.4),0.75);
+    	addSequential(new driveAuto(-0.5), 0.75);
+    	addSequential (new driveAuto(0),0.1);
+    
 	}
 
 	@Override
