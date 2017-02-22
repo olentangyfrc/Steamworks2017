fe1a3f888201099a34cfd653a94c0a5f990e7075
sethstobart <seth.stobart@gmail.com>
2017-02-21 20:50:57 -0500

    BOT TRIED TO KILL ME (AGAIN)

--- src/org/usfirst/frc/team4611/robot/commands/startRight.java
+++ src/org/usfirst/frc/team4611/robot/commands/startRight.java
@@ -5,14 +5,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class startRight extends CommandGroup{
 	
 	public startRight(){
+		//Hannah pneumatics close (extend) code goes here
 		addSequential(new driveAuto(-0.6),0.75);
     	addSequential(new turnAuto(-1, 0.55), 0.45);
     	addSequential(new driveAuto(-0.5), 0.3);
     	addSequential (new driveAuto(0),0.1);
     	addSequential(new autoAim(0.4), 2);
     	addSequential(new driveAuto(0), 0.1);
-    	addSequential(new ultraDrive(.5), 2);
+    	addSequential(new ultraDrive(.45), 2);
 		addSequential(new driveAuto(0),0.1);
+		//Hannah pneumatics open (retract) code goes here
+		//Hannah feeder also goes here
+		addSequential(new driveAuto(0.5), 1.5);
+		addSequential(new turnAuto(1, 0.55), 0.6);
+		addSequential (new driveAuto(0),0.1);	
 	}
 
 	@Override
