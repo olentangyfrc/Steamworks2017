94e67ca43a139b3f1d9b31ff5d060bbd159609fa
sethstobart <seth.stobart@gmail.com>
2017-03-07 15:16:01 -0500

    No seriously this time, there's feeder code

---
+++ src/org/usfirst/frc/team4611/robot/commands/autoFeeder.java
@@ -0,0 +1,24 @@
+package org.usfirst.frc.team4611.robot.commands;
+
+import org.usfirst.frc.team4611.robot.Robot;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+public class autoFeeder extends Command {
+	
+	public double speed;
+	
+	public autoFeeder (double s){
+		this.requires(Robot.fe);
+		this.speed = s;
+	}
+	protected void execute() {
+		Robot.fe.feed(this.speed);
+	}
+	
+	@Override
+	protected boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
