17b27d22699614b5af9236c4b879925676313705
sethstobart <seth.stobart@gmail.com>
2017-03-07 09:39:29 -0500

    startLeft set up, needs pnematics and feeder

--- src/org/usfirst/frc/team4611/robot/commands/startLeft.java
+++ src/org/usfirst/frc/team4611/robot/commands/startLeft.java
@@ -5,18 +5,21 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class startLeft extends CommandGroup{
 	
 	public startLeft(){
-		addSequential(new driveAuto(-0.5), 2);
-    	addSequential(new turnAuto(-1, 0.5), .5);
-    	addSequential(new driveAuto(-0.5), 1);
-    	addSequential(new driveAuto(0), 1);
-		addSequential(new driveAuto(-0.5),0.75);
-    	addSequential(new turnAuto(1, 0.45), 0.6);
-    	addSequential(new driveAuto(-0.5), 0.5);
+		//Hannah pneumatics close (extend) code goes here
+		addSequential(new driveAuto(-0.6),0.75);
+    	addSequential(new turnAuto(-1, 0.55), 0.45);
+    	addSequential(new driveAuto(-0.5), 0.3);
     	addSequential (new driveAuto(0),0.1);
-    	addSequential(new autoAim(0.4),0.75);
-    	addSequential(new driveAuto(-0.5), 0.75);
-    	addSequential (new driveAuto(0),0.1);
-
+    	addSequential(new autoAim(0.4), 2);
+    	addSequential(new driveAuto(0), 0.1);
+    	addSequential(new ultraDrive(.45), 2);
+		addSequential(new driveAuto(0),0.1);
+		//Hannah pneumatics open (retract) code goes here
+		//Hannah feeder also goes here
+		addSequential(new driveAuto(0.5), 1.5);
+		addSequential (new driveAuto(0),0.1);
+		addSequential(new turnAuto(1, 0.55), 0.6);
+		addSequential (new driveAuto(0),0.1);	
 	}
 
 	@Override
