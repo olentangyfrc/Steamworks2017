ecd1686aa1c8c856d2f06fad82d9e2ced2c83943
sethstobart <seth.stobart@gmail.com>
2017-03-12 16:49:49 -0400

    Added coomments and refactored autoAim

--- src/org/usfirst/frc/team4611/robot/commands/autoAim.java
+++ src/org/usfirst/frc/team4611/robot/commands/autoAim.java
@@ -14,23 +14,23 @@ public class autoAim extends Command {
 		
 	}
 	
-	public void moveContours(double x1, double x2){
+	public void moveContours(double x1, double x2){						//Receive the x values of our two target contours
 		double targetNum = 0;
-		targetNum = (Math.abs(x1 - x2)*0.625) + Math.max(x1, x2);
+		targetNum = (Math.abs(x1 - x2)*0.625) + Math.max(x1, x2);		//Set target location to the right of the right side contour. This will be 0.625 of the distance between the contours. This lines us up just about right, by trial and error
 		//System.out.println("Vision Speed:" + visionSpeed);
 		System.out.println("THIS IS target number:" + targetNum);
-		System.out.println("Average: " + x1+x2/2);
-		if (targetNum > 160){
+		System.out.println("Average: " + x1+x2/2);					//This says where the place between the contours is
+		if (targetNum > 160){							//If we're too right, then go more right to center the contours						
 			Robot.leftS.move(visionSpeed);
 			Robot.rightS.move(-visionSpeed);//theoretically move right
 			System.out.println("MOVED Right");
 		}
-		else if (targetNum < 160){
+		else if (targetNum < 160){						//If we're left, keep going more left to center the contours
 			Robot.leftS.move(-visionSpeed);
 			Robot.rightS.move(visionSpeed);
 			System.out.println("MOVED LEFT");
 		}
-		else{
+		else{											//If we're at exactly center by some miracle, don't move
 			Robot.leftS.move(0);	
 			Robot.rightS.move(0);
 		}
@@ -38,45 +38,40 @@ public class autoAim extends Command {
 	
 	
 	protected void execute() { 
-		double [] value = Robot.table.getNumberArray("centerX",new double [1]);
-		double [] value2 = Robot.table.getNumberArray("centerY",new double [1]);
+		System.out.println("autoAim started" + visionDone);
+		double [] xValues = Robot.table.getNumberArray("centerX",new double [1]);
+		double [] yValues = Robot.table.getNumberArray("centerY",new double [1]);
 		if(visionDone == false) {
-			System.out.println("Contours: "+value.length);
-			if(value.length > 0 && value2.length > 0) {
-				System.out.println("X value" +value[0]+ " Y value: " + value2[0]);
+			System.out.println("Number of Contours: "+xValues.length);
+			if(xValues.length > 0 && yValues.length > 0) {
+				System.out.println("X value #1: " +xValues[0]+ " Y value #1: " + yValues[0]);
 			}
-			int y1 = 0;
-			int y2 = 1;
-			if(value.length > 2) {
-				for(int c = 0; c < value2.length; c++) {
-					for(int c2 = c + 1; c2 < value2.length; c2++)
-					{
-						if(Math.abs(value2[c] - value2[c2]) <= 8) {
-							y1 = c;
-							y2 = c2;
+			int targetContour1 = 0;
+			int targetContour2 = 1;
+			if(xValues.length > 2) {									//These loops go through and find the first 2 contours on a similar vertical location
+				for(int c = 0; c < yValues.length; c++) {				//Please kill me. You can do this with one loop.
+					for(int c2 = c + 1; c2 < yValues.length; c2++){
+						if(Math.abs(yValues[c] - yValues[c2]) <= 8) {
+							targetContour1 = c;		//Set of 2 contours that we want since they're close together vertically.
+							targetContour2 = c2;	//Second of the two. These are the array locations of the elements that define the contours we want
 						}
-						
 					}
 				}
 			}
 		
 			
-			if(value.length >= 2) {
-				moveContours(value[y1], value[y2]);
+			if(xValues.length >= 2) {							//If we have 2 or more contours, then just run the normal contours command
+				moveContours(xValues[targetContour1], xValues[targetContour2]);			//Remember, these are the array locations
 			}
-			else if(value.length == 1 && value[0] < 100) {
-				Robot.leftS.move(-visionSpeed);
+			else if(xValues.length == 1 && xValues[0] < 100) {	//If there's only one contour, and it's to the left of 100, then turn a bit to the left
+				Robot.leftS.move(-visionSpeed);					//Maybe this is when we're only finding one contour because the other is offscreen?
 				Robot.rightS.move(visionSpeed);
 			}
-			else {
+			else {												//If we don't have any contours, then stop moving and say we're done. Why?
 				Robot.leftS.move(0);
-				Robot.rightS.move(0);
-				//visionDone = true;
-				System.out.println("VISION DONE");
+				Robot.rightS.move(0);							//Build a better case for what to do when we can't find anything. Probably turn in place.
+				System.out.println("NO CONTOURS FOUND");
 			}
-			}
-		else{
-			return;
 		}
 	}
 
