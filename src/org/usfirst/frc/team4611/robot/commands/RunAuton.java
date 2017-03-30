8b82fc67368034f0d3040bc4f2cf8b0ec0d4b81f
Hannah He <hannahhe18@gmail.com>
2017-03-30 15:31:45 -0400

    fixed left and right side for auton

--- src/org/usfirst/frc/team4611/robot/commands/RunAuton.java
+++ src/org/usfirst/frc/team4611/robot/commands/RunAuton.java
@@ -18,12 +18,12 @@ public class RunAuton extends CommandGroup {
 			addSequential(new ExtendSolenoid(), 1); //Open solenoid	
 			addParallel(new MoveFeeder(-0.75), 1); //Run the feeder
 			addSequential(new driveAuto(-0.5), 1.5); //Drive backwards
-			addSequential(new driveAuto(0),0.1); //Stop driving backwards
+			addSequential (new driveAuto(0),0.1); //Stop driving backwards
 			addParallel(new MoveFeeder(0), 0.1); //Stop running the feeder
 			addParallel(new RetractSolenoid(), 1); //Close the solenoid 
 			addSequential(new turnAuto(1, 0.75), 0.65); //Turn left
 			addSequential(new driveAuto(0.75), 1.5);
-			addSequential(new driveAuto(0), 2); //go to center of field
+			addSequential(new driveAuto(0), 2);
 			break;
 		case MIDDLE:
 			addSequential(new driveAuto(0.75), 0.8); //Drive forward
@@ -39,23 +39,20 @@ public class RunAuton extends CommandGroup {
 			addParallel(new RetractSolenoid(), 1); //Close the solenoid 
 			break;
 		case RIGHT:
-			addSequential(new driveAuto(0.6),0.75); //First step: drive forward at 60% for 0.75 seconds
-	    	addSequential(new turnAuto(-1, 0.55), 0.45); //Second step: turn left at 55% for 0.45 seconds 
-	    	addSequential(new driveAuto(0.5), 0.3); //Third step: drive forward at 50% for 0.3 seconds
-	    	addSequential (new driveAuto(0),0.1); //Fourth step: stop for 0.1 second
-	    	addSequential(new autoAim(0.4), 2); 
+			addSequential(new driveAuto(0.75), 1.3); //Drive forward
+	    	addSequential(new turnAuto(1, 0.55), 0.6); //Turn left
 	    	addSequential(new driveAuto(0), 0.1);
 	    	addSequential(new driveAuto(0.75), 0.7);
 	    	addSequential(new driveAuto(0), 1.75);
 	    	addSequential(new autoAim(0.4), 2); //Turn using VA
 	    	addSequential(new ultraDrive(-0.45), 2); //Go forward until you hit
-			addSequential(new ExtendSolenoid(), 1); //Open solenoid
+			addSequential(new ExtendSolenoid(), 1); //Open solenoid		S
 			addParallel(new MoveFeeder(-0.75), 1); //Run the feeder
 			addSequential(new driveAuto(-0.5), 1.5); //Drive backwards
 			addSequential (new driveAuto(0),0.1); //Stop driving backwards
 			addParallel(new MoveFeeder(0), 0.1); //Stop running the feeder
 			addParallel(new RetractSolenoid(), 1); //Close the solenoid 
-			addSequential(new turnAuto(1, 0.75), 0.65); //Turn right
+			addSequential(new turnAuto(-1, 0.75), 0.65); //Turn right
 			addSequential(new driveAuto(0.75), 1.5);
 			addSequential(new driveAuto(0), 2);
 			break;
