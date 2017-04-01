0a6a644ca4e2fe4c90a8f4de02e252e83180d43c
Hannah He <hannahhe18@gmail.com>
2017-03-31 20:57:28 -0400

    removed VA from auton, needs testing

--- src/org/usfirst/frc/team4611/robot/commands/startLeft.java
+++ src/org/usfirst/frc/team4611/robot/commands/startLeft.java
@@ -5,29 +5,13 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class startLeft extends CommandGroup{
 	
 	public startLeft(){
-		addSequential(new driveAuto(0.6), 0.85);
-		//was 1 second for third match
-		//Drive forward //was 1.3 seconds for first match
-    	addSequential(new turnAuto(-1, 0.45), 0.3); //Turn right //was 0.4 seconds for third match
-    	addSequential(new driveAuto(0), 0.1);
-    	//addSequential(new driveAuto(0.4), 0.15); // was 0.6 power second match //was 0.5 seconds for first match //after turn, get closer to peg to initiate VA
-    	//^was 0.3 seconds second match
-    	//addSequential(new driveAuto(0), 1.75);
-    	//addSequential(new autoAim(0.25), 4); //Turn using VA //was 2 seconds third match
-    	addSequential(new ultraDrive(-0.45), 3); //Go forward until you hit
+		addSequential(new driveAuto(0.6), 1); //Drive forward //was 1.3 seconds for first match
+		addSequential(new turnAuto(-1, 0.45), 0.4); //Turn right
+    	addSequential(new ultraDrive(-0.45), 1.3); //Go forward until you hit
     	addSequential(new driveAuto(0), 0.1); //stop driving
+		
+		//Copy of startRight() with reversed turn direction
     	
-    	//Schmidt didn't want us to continue past getting the gear on the peg
-    	/*addSequential(new driveAuto(0), 0.1); //Stop driving
-		addSequential(new ExtendSolenoid(), 1); //Open solenoid	
-		addParallel(new MoveFeeder(-0.75), 1); //Run the feeder
-		addSequential(new driveAuto(-0.5), 1.5); //Drive backwards
-		addSequential (new driveAuto(0),0.1); //Stop driving backwards
-		addParallel(new MoveFeeder(0), 0.1); //Stop running the feeder
-		addParallel(new RetractSolenoid(), 1); //Close the solenoid 
-		addSequential(new turnAuto(1, 0.55), 0.65); //Turn left
-		addSequential(new driveAuto(0.55), 1.5);
-		addSequential(new driveAuto(0), 2);*/
 	}
 
 	@Override
--- src/org/usfirst/frc/team4611/robot/commands/startRight.java
+++ src/org/usfirst/frc/team4611/robot/commands/startRight.java
@@ -5,27 +5,22 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
 public class startRight extends CommandGroup{
 	
 	public startRight(){
-		addSequential(new driveAuto(0.6), 0.85); //Drive forward //was 1.3 seconds for first match
+		addSequential(new driveAuto(0.6), 1); //Drive forward //was 1.3 seconds for first match
+		addSequential(new turnAuto(1, 0.45), 0.4); //Turn left
+    	addSequential(new ultraDrive(-0.45), 1.3); //Go forward until you hit
+    	addSequential(new driveAuto(0), 0.1); //stop driving
+    	
+    	//Nearly functional match 2 code
+    	/*addSequential(new driveAuto(0.6), 1); //Drive forward //was 1.3 seconds for first match
 		//was 1 second for third match
 		addSequential(new turnAuto(1, 0.45), 0.4); //Turn left
     	addSequential(new driveAuto(0), 0.1);
-    	//addSequential(new driveAuto(0.4), 0.15); //was 0.6 power second match //was 0.5 seconds for first match//after turn, get closer to peg to initiate VA
+    	addSequential(new driveAuto(0.6), 0.3); //was 0.6 power second match //was 0.5 seconds for first match//after turn, get closer to peg to initiate VA
     	//^was 0.3 seconds second match
-    	//addSequential(new driveAuto(0), 1.75);
-    	//addSequential(new autoAim(0.25), 4); //Turn using VA ////was 2 seconds third match
-    	addSequential(new ultraDrive(-0.45), 3); //Go forward until you hit
-    	addSequential(new driveAuto(0), 0.1); //stop driving
-    	
-    	//Schmidt didn't want us to continue past getting the gear on the pe g
-    	/*addSequential(new ExtendSolenoid(), 1); //Open solenoid	
-		addParallel(new MoveFeeder(-0.75), 1); //Run the feeder
-		addSequential(new driveAuto(-0.5), 1.5); //Drive backwards
-		addSequential (new driveAuto(0),0.1); //Stop driving backwards
-		addParallel(new MoveFeeder(0), 0.1); //Stop running the feeder
-		addParallel(new RetractSolenoid(), 1); //Close the solenoid 
-		addSequential(new turnAuto(-1, 0.55), 0.65); //Turn right
-		addSequential(new driveAuto(0.55), 1.5);
-		addSequential(new driveAuto(0), 2);*/
+    	addSequential(new driveAuto(0), 1.75);
+    	addSequential(new autoAim(0.4), 2); //Turn using VA ////was 2 seconds third match
+    	addSequential(new ultraDrive(-0.45), 1); //Go forward until you hit
+    	addSequential(new driveAuto(0), 0.1); //stop driving*/
 	}
 
 	@Override
